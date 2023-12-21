7a53bba224e339c3df89d51a73304d7dacd27e11
Martin Kapal <flamecze@gmail.com>
2023-12-21 23:30:09 +0100

    Initial commit

---
+++ index.ts
@@ -0,0 +1,30 @@
+import { InSim } from 'node-insim';
+import { IS_ISI_ReqI, type IS_VER, PacketType } from 'node-insim/packets';
+
+const inSim = new InSim();
+
+inSim.connect({
+  IName: 'Node InSim App',
+  Host: '127.0.0.1',
+  Port: 29999,
+  ReqI: IS_ISI_ReqI.SEND_VERSION,
+  Admin: '',
+});
+
+inSim.on('connect', () => {
+  console.log('Connected');
+});
+
+inSim.on('disconnect', () => {
+  console.log('Disconnected');
+});
+
+inSim.on(PacketType.ISP_VER, onVersion);
+
+function onVersion(packet: IS_VER) {
+  console.log(`Connected to LFS ${packet.Product} ${packet.Version}`);
+}
+
+process.on('uncaughtException', (error) => {
+  console.log(error);
+});
